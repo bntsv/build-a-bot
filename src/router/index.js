a3ce670f91744a2f6a39abc0a168699cb273fb7b
bntsv <you@example.com>
2024-04-24 19:13:36 +0300

    Add ProductSearch and fix router issues

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { createRouter } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomePage from "../home/HomePage.vue";
 import RobotBuilder from "../build/RobotBuilder.vue";
+import ProductSearch from "../search/ProductSearch.vue";
 
 export default createRouter({
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
@@ -14,6 +16,11 @@ export default createRouter({
       path: "/build",
       name: "Build",
       component: RobotBuilder,
+    },
+    {
+      path: "/search",
+      name: "Search",
+      component: ProductSearch,
     },
   ],
 });
